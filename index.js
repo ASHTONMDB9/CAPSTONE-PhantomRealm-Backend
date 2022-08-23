d5e239422df0ef7e4ced901994a09b5007d6a2af
ASHTONMDB9 <ashtonabrahams563@gmail.com>
2022-08-23 13:11:10 +0200

    displayed the routes and explaination using static

--- index.js
+++ index.js
@@ -5,9 +5,13 @@ const app = express(); // Initialize express as an app variable
 app.set("port", process.env.PORT || 4427); // Set the port
 app.use(express.json()); // Enable the server to handle JSON requests
 app.use(cors()); // Dont let local development give errors
+app.use(express.static('public')); 
+
+//Serves all the request which includes /images in the url from Images folder 
+app.use('/index.html', express.static(__dirname + '/index.html'));
 
 app.get("/", (req, res) => {
-    res.json({ msg: "Welcome" });
+    res.sendFile(__dirname + "/" + "index.html");
 });
 
 app.use('/users', require('./routes/users'))
