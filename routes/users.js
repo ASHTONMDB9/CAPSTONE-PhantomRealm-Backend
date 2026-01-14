3e6fd40ff1866f4a10ba7acca444dda78ed847cd
ASHTONMDB9 <ashtonabrahams563@gmail.com>
2026-01-14 12:06:05 +0200

    forget/reset III

--- routes/users.js
+++ routes/users.js
@@ -4,7 +4,6 @@ const bcrypt = require('bcryptjs');
 const con = require("../lib/db_connection");
 const jwt = require('jsonwebtoken')
 const middleware = require("../middleware/auth");
-// const transporter = require("../utils/email");
 
 router.post("/register", (req, res) => {
     try {
